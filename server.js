7eab74ffdd93d7380a9b772cbb4e8b9997ff037d
Isha Roy <isharoy@Ishas-MacBook-Air.local>
2026-02-01 23:15:34 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+
+const app = express()
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send("hello world!");
+})
+
+app.post('/notes',(req,res)=>{
+    const{title,content} = req.body;
+    console.log(title,content)
+    
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
+})
