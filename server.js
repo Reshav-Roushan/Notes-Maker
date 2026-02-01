7a1940266c519caa2b420903127502494aff6ed4
Reshav-Roushan <reshav1995@gmail.com>
2026-02-02 00:57:21 +0530

    Crud implemented

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ connectToDB()
 const app = express() //Initial
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send("hello world!");
-})
 
+
+//Create operation
 app.post('/notes',async (req,res)=>{
     const{title,content} = req.body;
     console.log(title,content)
@@ -22,6 +21,42 @@ app.post('/notes',async (req,res)=>{
         message:"Note created sucessfully"
     })
     
+})
+
+//Read operation
+app.get('/notes',async(req,res)=>{
+    const notes = await noteModel.find()
+    res.json({
+        message:"Notes fetch successfully",
+        notes
+    })
+})
+
+//delete operation
+app.delete("/notes/:id",async(req,res)=>{
+    const noteId = req.params.id
+
+    await noteModel.findOneAndDelete({
+        _id : noteId
+    })
+    res.json({
+        message:"Note Deleted"
+    })
+})
+
+//update operation
+app.patch("/notes/:id",async(req,res)=>{
+    const noteId = req.params.id
+    const {title} = req.body
+    
+    await noteModel.findOneAndUpdate({
+        _id : noteId
+    },{
+        title: title
+    })
+    res.json({
+        message:"Note updated"
+    })
 })
 
 
