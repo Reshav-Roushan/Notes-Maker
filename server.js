75a2795e62efdd1302cb49a4f46c676404f04601
Reshav-Roushan <reshav1995@gmail.com>
2026-02-02 00:31:14 +0530

    Database creation

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-const express = require('express');
+const express = require('express'); //Initial
 const connectToDB = require('./src/db/db')
-
+const noteModel = require('./src/models/note.model')
 
 
 connectToDB()
-const app = express()
+const app = express() //Initial
 app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send("hello world!");
 })
 
-app.post('/notes',(req,res)=>{
+app.post('/notes',async (req,res)=>{
     const{title,content} = req.body;
     console.log(title,content)
+
+    await noteModel.create({
+        title,content
+    })
+    res.json({
+        message:"Note created sucessfully"
+    })
     
 })
 
 
-
+//Initial
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
 })
